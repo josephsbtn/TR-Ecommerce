3eaa27c6c9be455a978cf93e579af9d7574c0c7f
anitaOca <672023228@student.uksw.edu>
2024-11-29 23:51:20 +0700

    Update ItemCard.js
    oke

--- client/src/component/design/ItemCard.js
+++ client/src/component/design/ItemCard.js
@@ -5,9 +5,10 @@ function ItemCard({ item }) {
   return (
     <>
       <Link
-        to={user.isAdmin ? `/editItem/${item._id}` : `/detailItem/${item._id}`}>
-        <div className="bg-white w-60 h-auto flex flex-col mt-2 p-2 rounded-2xl shadow-xl ">
-          <img src={item.image} className="w-60 h-44 mt-4" />
+        to={user.isAdmin ? `/editItem/${item._id}` : `/detailItem/${item._id}`}
+      >
+        <div className="bg-white w-60 h-full flex flex-col mt-2 p-2 rounded-2xl shadow-xl ">
+          <img src={item.image} className="w-48 h-44 m4" />
           <h1 className="ml-4 font-montserrat text-sm font-medium">
             {item.name}
           </h1>
