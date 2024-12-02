ef63dd4e0f50b52ca756612c6764ebf81d72e8bb
anitaOca <672023228@student.uksw.edu>
2024-12-03 01:24:28 +0700

    image

--- client/src/screen/aboutus.js
+++ client/src/screen/aboutus.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import Navbar from "../component/design/navbar";
 import SideNavUser from "../component/design/SideNavUser";
 import image1 from "../component/image/image1AboutUs.png";
+import image2 from "../component/image/image2AboutUs.png";
+import image3 from "../component/image/image3AboutUs.png";
 
 function Aboutus() {
   const [open, setOpen] = useState(false);
@@ -15,14 +17,10 @@ function Aboutus() {
 
         <div
           className="h-auto w-full bg-white mt-16"
-          onClick={() => setOpen(false)}>
+          onClick={() => setOpen(false)}
+        >
           <div className="max-w-7xl mx-auto p-8">
             <div className="text-center">
-              <img
-                src={image1}
-                alt="Rose of Sharon Logo"
-                className="mx-auto w-32 mb-4"
-              />
               <h2 className="text-gray-700 text-xl font-roman tracking-wide">
                 OUR MOTTO
               </h2>
@@ -36,14 +34,17 @@ function Aboutus() {
 
             <div className="relative mt-16">
               <div className="w-full flex justify-end">
-                <div className="absolute w-1/3">
+                <div className="absolute w-1/3 z-0">
+                  {" "}
+                  {/* Ensure this element is behind the navbar */}
                   <img
                     src={image1}
                     alt="Jewelry Image"
-                    className="rounded-bl-[80px] overflow-hidden object-cover"
+                    className=" rounded-bl-[80px] overflow-hidden object-cover"
                   />
                   <div className="absolute border-t-4 border-l-4 border-yellow-600 rounded-bl-[80px] h-full w-full top-0 left-0 -z-10"></div>
                 </div>
+                  
               </div>
             </div>
 
@@ -63,9 +64,9 @@ function Aboutus() {
                   {/* Left Image */}
                   <div className="w-full lg:w-1/2">
                     <img
-                      src="/path-to-left-image.png"
+                      src={image2}
                       alt="Rose of Sharon Jewelry"
-                      className="rounded-xl object-cover w-full h-full"
+                      className=" w-96 h-80object-cover shadow-md mt-16 "
                     />
                   </div>
 
@@ -85,9 +86,9 @@ function Aboutus() {
                 <div className="mt-12 flex justify-center">
                   <div className="w-full lg:w-1/2">
                     <img
-                      src="/path-to-bottom-image.png"
+                      src={image3}
                       alt="Jewelry Hand Image"
-                      className="rounded-xl object-cover w-full h-full"
+                      className=" -mt-60 w-64 h-60 object-cover bottom-[-30px] right-[-20px]"
                     />
                   </div>
                 </div>
